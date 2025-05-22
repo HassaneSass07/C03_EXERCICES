230e0c114846f29541a5833629bfa7dfe35f6faf
HassaneSass07 <sasshassane02@gmail.com>
2025-05-22 03:43:31 +0100

    netw

--- reverse_number.c
+++ reverse_number.c
@@ -3,8 +3,10 @@
 int main() {
     //Déclaration des variables
     int nombre, inverse = 0;
+
 //Lire la saisie utilisateur
     scanf("%d", &nombre);
+
 //Boucle permettant d'inverser les chiffres d'un entier
     while (nombre != 0) {
         inverse = inverse * 10 + nombre % 10;
@@ -13,4 +15,4 @@ int main() {
 
     printf("Nombre inversé : %d\n", inverse);
     return 0;
-}
+}a
